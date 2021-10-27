ad7664a078c65860401d2ecfcd5720cc59d8848f
marble <kangsw1025@naver.com>
2021-10-27 23:35:36 +0900

    Update Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../routes/Home";
 import Reservation from "../routes/Reservation";
+import ReservationCheck from "../routes/ReservationCheck";
+import ReservationForm from "../routes/ReservationForm";
 
 export default () => {
   return (
@@ -10,8 +12,14 @@ export default () => {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/reservation">
+        <Route path="/book">
           <Reservation />
+        </Route>
+        <Route path="/bookCheck">
+          <ReservationCheck />
+        </Route>
+        <Route path="/bookForm">
+          <ReservationForm />
         </Route>
       </Switch>
     </Router>
